dc529d1b47bb9b289b0469a2970bf79885f0d0a2
Lucas Pereira Miranda <lucas.miranda1@aluno.ufop.edu.br>
2021-02-15 20:26:39 -0300

    develop update method

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -2,6 +2,7 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
 import { Product } from './products.entity';
 
 @Injectable()
@@ -36,5 +37,14 @@ export class ProductsService {
     const product = await this.findOne(id);
     await this.productRepository.delete(id);
     return product;
+  }
+
+  async update(
+    id: number,
+    updateProductDto: UpdateProductDto,
+  ): Promise<Product> {
+    await this.findOne(id);
+    await this.productRepository.update({ id }, updateProductDto);
+    return this.findOne(id);
   }
 }
