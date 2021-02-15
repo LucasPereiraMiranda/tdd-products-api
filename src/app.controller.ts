d6d23b32abf337ade67c8b20c29a81eb33930158
Lucas Pereira Miranda <lucas.miranda1@aluno.ufop.edu.br>
2021-02-15 09:50:16 -0300

    change response type for return WelcomeResponse

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { WelcomeResponse } from './interface/app.interface';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHello(): WelcomeResponse {
     return this.appService.getHello();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { WelcomeResponse } from './interface/app.interface';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): WelcomeResponse {
+    return { message: 'Welcome to nestjs tdd-products-api' };
   }
 }
