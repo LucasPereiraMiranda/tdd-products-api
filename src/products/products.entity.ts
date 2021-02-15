904426cb244361608c52df0cc02c44b910a9504f
Lucas Pereira Miranda <lucas.miranda1@aluno.ufop.edu.br>
2021-02-15 13:23:21 -0300

    adding products entity

---
+++ src/products/products.entity.ts
@@ -0,0 +1,27 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+} from 'typeorm';
+
+@Entity('products')
+export class Product {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column({ unsigned: true })
+  value: number;
+
+  @Column()
+  description: string;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt: Date;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  updatedAt: Date;
+}
