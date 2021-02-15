0f4447a70ae368320028fe202ea5a0d4b4ef896e
Lucas Pereira Miranda <lucas.miranda1@aluno.ufop.edu.br>
2021-02-15 10:03:07 -0300

    Import ConfigModule & TypeOrmModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { resolve } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
-
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      // @ts-ignore
+      type: process.env.TYPEORM_CONNECTION,
+      host: process.env.TYPEORM_HOST,
+      port: parseInt(process.env.TYPEORM_PORT),
+      username: process.env.TYPEORM_USERNAME,
+      password: process.env.TYPEORM_PASSWORD,
+      database: process.env.TYPEORM_DATABASE,
+      entities: [resolve(__dirname, '**', '*.entity{.ts,.js}')],
+    }),
+    ProductsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
