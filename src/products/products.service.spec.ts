efb26528fdada251618da9c143dfe84e907c3db6
Lucas Pereira Miranda <lucas.miranda1@aluno.ufop.edu.br>
2021-02-15 15:29:20 -0300

    refactor and create findAll test

--- src/products/products.service.spec.ts
+++ src/products/products.service.spec.ts
@@ -5,6 +5,19 @@ import { CreateProductDto } from './dto/create-product.dto';
 import { Product } from './products.entity';
 import { ProductsService } from './products.service';
 
+const product1: Product = new Product(
+  'Smartphone',
+  9999.9,
+  'The best Smatphone in the world!',
+);
+const product2: Product = new Product(
+  'Notebook',
+  150000.0,
+  'The best Computer in the world!',
+);
+
+const productArray = [product1, product2];
+
 describe('ProductsService', () => {
   let productsService: ProductsService;
   let productsRepository: Repository<Product>;
@@ -16,9 +29,11 @@ describe('ProductsService', () => {
         {
           provide: getRepositoryToken(Product),
           useValue: {
-            create: jest.fn(),
+            find: jest.fn().mockResolvedValue(productArray),
+            create: jest.fn().mockResolvedValue(product1),
             save: jest.fn(),
-            findOne: jest.fn(),
+            findOne: jest.fn().mockResolvedValue(product1),
+
             metadata: {
               columns: [
                 { propetyName: 'id', isPrimary: true },
@@ -43,13 +58,23 @@ describe('ProductsService', () => {
 
   describe('create()', () => {
     it('should create a product', async () => {
-      const createProductDto = {
+      const createProduct1Dto = {
         name: 'Smartphone',
-        value: 8990.99,
-        description: 'The best smartphone in the world!',
+        value: 9999.9,
+        description: 'The best Smatphone in the world!',
       };
-      await productsService.create(createProductDto);
-      expect(productsRepository.create).toBeCalledWith(createProductDto);
+      expect(productsService.create(createProduct1Dto)).resolves.toEqual(
+        product1,
+      );
+      expect(productsRepository.create).toBeCalledTimes(1);
+      expect(productsRepository.create).toBeCalledWith(createProduct1Dto);
+    });
+  });
+
+  describe('findAll()', () => {
+    it('should find all products, returning a array', async () => {
+      const products = await productsService.findAll();
+      expect(products).toEqual(productArray);
     });
   });
 });
