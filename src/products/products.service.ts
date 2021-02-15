ed46aeb4d3d1a99b605aaee0470313b662959f88
Lucas Pereira Miranda <lucas.miranda1@aluno.ufop.edu.br>
2021-02-15 13:25:55 -0300

    develop products service

---
+++ src/products/products.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateProductDto } from './dto/create-product.dto';
+import { Product } from './products.entity';
+
+@Injectable()
+export class ProductsService {
+  constructor(
+    @InjectRepository(Product)
+    private productRepository: Repository<Product>,
+  ) {}
+
+  async create(createProductDto: CreateProductDto): Promise<Product> {
+    const { name, value, description } = createProductDto;
+    const product = new Product();
+    product.name = name;
+    product.value = value;
+    product.description = description;
+    return this.productRepository.create(createProductDto);
+  }
+}
