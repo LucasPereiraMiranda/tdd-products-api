a3e5c1732494482f4f8b43403932f73f1c7b8830
Lucas Pereira Miranda <lucas.miranda1@aluno.ufop.edu.br>
2021-02-15 21:16:19 -0300

    implement create in controller

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
 import { Product } from './products.entity';
 import { ProductsService } from './products.service';
 
@@ -9,5 +10,10 @@ export class ProductsController {
   @Get()
   async findAll(): Promise<Product[]> {
     return await this.productsService.findAll();
+  }
+
+  @Post()
+  create(@Body() createProductDto: CreateProductDto) {
+    return this.productsService.create(createProductDto);
   }
 }
