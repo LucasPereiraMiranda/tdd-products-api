0be99533806272574f4db6b9b53fe473954d6080
Lucas Pereira Miranda <lucas.miranda1@aluno.ufop.edu.br>
2021-02-15 21:02:47 -0300

    implement test for findAll in controller spec

---
+++ src/products/tests/products.controller.spec.ts
@@ -0,0 +1,79 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { CreateProductDto } from '../dto/create-product.dto';
+import { UpdateProductDto } from '../dto/update-product.dto';
+import { ProductsController } from '../products.controller';
+import { ProductsService } from '../products.service';
+
+describe('ProductsController', () => {
+  let controller: ProductsController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [ProductsController],
+      providers: [
+        {
+          provide: ProductsService,
+          useValue: {
+            findAll: jest.fn().mockResolvedValue([
+              {
+                name: 'Smartphone',
+                value: 9999.9,
+                description: 'The best Smatphone in the world!',
+              },
+              {
+                name: 'Notebook',
+                value: 150000.0,
+                description: 'The best Notebook in the world!',
+              },
+            ]),
+            findOne: jest.fn().mockImplementation((id: string) =>
+              Promise.resolve({
+                name: 'Notebook',
+                value: 150000.0,
+                description: 'The best Notebook in the world!',
+                id,
+              }),
+            ),
+            create: jest
+              .fn()
+              .mockImplementation((createProductDto: CreateProductDto) =>
+                Promise.resolve({ id: 1, ...createProductDto }),
+              ),
+            update: jest
+              .fn()
+              .mockImplementation(
+                (id: string, updateProductDto: UpdateProductDto) =>
+                  Promise.resolve({ id, ...updateProductDto }),
+              ),
+            remove: jest
+              .fn()
+              .mockImplementation((id: string) => Promise.resolve({ id })),
+          },
+        },
+      ],
+    }).compile();
+
+    controller = module.get<ProductsController>(ProductsController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+
+  describe('findAll()', () => {
+    it('should find one array of products', async () => {
+      await expect(controller.findAll()).resolves.toEqual([
+        {
+          name: 'Smartphone',
+          value: 9999.9,
+          description: 'The best Smatphone in the world!',
+        },
+        {
+          name: 'Notebook',
+          value: 150000.0,
+          description: 'The best Notebook in the world!',
+        },
+      ]);
+    });
+  });
+});
