bd9270c714ed4da4d96470c74cda503d0d03e2bc
Lucas Pereira Miranda <lucas.miranda1@aluno.ufop.edu.br>
2021-02-15 21:03:38 -0300

    adding controller in products module

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from './products.entity';
 import { ProductsService } from './products.service';
+import { ProductsController } from './products.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Product])],
   providers: [ProductsService],
+  controllers: [ProductsController],
 })
 export class ProductsModule {}
