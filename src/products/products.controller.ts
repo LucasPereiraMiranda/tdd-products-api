3946f32cbb1e8fe7e8df9f240c9a73f011cf15d2
Lucas Pereira Miranda <lucas.miranda1@aluno.ufop.edu.br>
2021-02-15 21:37:34 -0300

    develop findOne & update in products controller

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
 import { Product } from './products.entity';
 import { ProductsService } from './products.service';
 
@@ -15,5 +16,15 @@ export class ProductsController {
   @Post()
   create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.productsService.findOne(+id);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
+    return this.productsService.update(+id, updateProductDto);
   }
 }
