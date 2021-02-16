7401b38ee1e517f1afe148685851038c569f1708
Lucas Pereira Miranda <lucas.miranda1@aluno.ufop.edu.br>
2021-02-15 21:03:18 -0300

    implement method findAll in controller

---
+++ src/products/products.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from '@nestjs/common';
+import { Product } from './products.entity';
+import { ProductsService } from './products.service';
+
+@Controller('products')
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  @Get()
+  async findAll(): Promise<Product[]> {
+    return await this.productsService.findAll();
+  }
+}
